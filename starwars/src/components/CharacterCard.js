d8bbb9614d6b9329a9f3d3839ae31e6066d99b06
jkuenzinger <jkuenzinger228@gmail.com>
2020-01-24 10:40:13 -0800

    got it working did some basica styling broke the api tryin gto work with it to get styled in time

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,14 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
+export const StarCard = styled.div`
+display:flex;
+width:40%;
+flex-direction: column;
+align-items: center;
+justify-content: space-between;
+justify-content: center;
+border-style: solid 1px;
+margin-left: 30%;
+
+`;
 
 const CharacterCard = ({item}) => {
     return (
-        <div>
+        <StarCard>
             <h2>Name: {item.name}</h2>
             <p>Hair Color:{item.hair_color}</p>
             <p>Height:{item.height}</p>
             <p>Mass:{item.mass}</p>
            
-           </div>
+           </StarCard>
     );
 };
 
